cf1f58ae6919c58c6fca52f62614f5e8cc31d9c2
Furt_Tech <none@none.com>
2023-12-28 06:54:34 -0600

    added form  based crud functions and validation for customer invoices

--- dashboard-nextjs/app/ui/dashboard/cards.tsx
+++ dashboard-nextjs/app/ui/dashboard/cards.tsx
@@ -5,6 +5,7 @@ import {
   InboxIcon,
 } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import { fetchCardData } from '../../lib/data';
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -14,18 +15,20 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
+  
+  const fetchCardQuery = (await fetchCardData());
+  
   return (
     <>
       {/* NOTE: comment in this code when you get to this point in the course */}
-
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
-      <Card title="Pending" value={totalPendingInvoices} type="pending" />
-      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-      <Card
-        title="Total Customers"
-        value={numberOfCustomers}
-        type="customers"
-      /> */}
+      { <Card title="Collected" value={fetchCardQuery.totalPaidInvoices} type="collected" /> }
+        { <Card title="Pending" value={fetchCardQuery.totalPendingInvoices} type="pending" /> }
+        { <Card title="Total Invoices" value={fetchCardQuery.numberOfInvoices} type="invoices" /> }
+        { <Card
+          title="Total Customers"
+          value={fetchCardQuery.numberOfCustomers}
+          type="customers"
+        /> }
     </>
   );
 }
