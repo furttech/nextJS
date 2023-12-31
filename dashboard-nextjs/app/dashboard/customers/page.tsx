dde329a601989d4d6d8b17310f162f202dac2de0
Furt_Tech <none@none.com>
2023-12-30 23:49:06 -0600

    Finished Tutorial Walk Thru. Code Base Suitable for expansion.

--- dashboard-nextjs/app/dashboard/customers/page.tsx
+++ dashboard-nextjs/app/dashboard/customers/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: 'Customer Manager',
+}
+
+
 export default function Customers(){
     return (<p>Customers Page!</p>);
 }
